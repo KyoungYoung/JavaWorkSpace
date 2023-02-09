81fe833e1ebeeca56572374e852232a64c427f21
kky <kky9616tt@gmail.com>
2023-02-10 07:07:25 +0900

    day07-1

--- src/FastCampusJava1/Hihi.java
+++ src/FastCampusJava1/Hihi.java
@@ -1,7 +1,22 @@
 package FastCampusJava1;
 
+import java.util.Arrays;
+
 public class Hihi {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int a, b, sum,minus,multiply,divide;
+        a = 12;
+        b = 2;
+        sum = a + b;
+        minus = a - b;
+        multiply = a * b;
+        divide = a / b;
+        System.out.println("a + b = " + sum);
+        System.out.println("a - b = " + minus);
+        System.out.println("a * b = " + multiply);
+        System.out.println("a / b = " + divide);
     }
+
+
+
 }
---
+++ src/FastCampusJava1/Operator.java
@@ -0,0 +1,10 @@
+package FastCampusJava1;
+
+public class Operator {
+    public static void main(String[] args) {
+        int korean = 77;
+        int english = 87;
+        System.out.println("총점: " + (korean + english));
+        System.out.println("평균: " + ((korean + english) / 2));
+    }
+}
