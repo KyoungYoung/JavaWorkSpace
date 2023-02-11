c5f4ab5fdf029fa4684830d4d6436825e50d17c1
kky <kky9616tt@gmail.com>
2023-02-11 23:50:45 +0900

    day08

---
+++ src/FastCampusJava1/HelloTest.java
@@ -0,0 +1,23 @@
+package FastCampusJava1;
+
+public class HelloTest {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        String aa = Integer.toString(a);
+        String ab = String.valueOf(a);
+        System.out.println(aa+10);
+        System.out.println(ab+10);
+
+        int c = 10;
+        int d = 20;
+        int temp;
+        temp = c;
+        c = d;
+        d = temp;
+        System.out.println(temp);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
